729f5aad8dbf20433c53e83cb537f85782e577a8
Rejoan Ahmed <122503628+Niropoma@users.noreply.github.com>
2023-02-27 22:16:34 +0600

    Add files via upload

---
+++ linkshort.py
@@ -0,0 +1,41 @@
+import os
+import time
+import pyshorteners
+user = "rejoan"
+password = "1212"
+logo = '''
+ _______    ______   __    __ 
+/       \  /      \ /  |  /  |
+$$$$$$$  |/$$$$$$  |$$ | /$$/ 
+$$ |__$$ |$$____$$ |$$ |/$$/  
+$$    $$<  /    $$/ $$  $$<   
+$$$$$$$  |/$$$$$$/  $$$$$  \  
+$$ |  $$ |$$ |_____ $$ |$$  \ 
+$$ |  $$ |$$       |$$ | $$  |
+$$/   $$/ $$$$$$$$/ $$/   $$/ 
+                              
+                              
+                              
+'''
+
+givenuser = input("Enter your user: ")
+if givenuser == user:
+  print("Correct user")
+  givenpassword = input("Enter Your password: ")
+  if givenpassword == password:
+   print(" Wellcome to this tool")
+   
+  else:
+    print("incorrect password")
+    time.sleep(2)
+else:
+  print("Wrong user")
+  
+
+link = input(" Enter your link")
+s = pyshorteners.Shortener()
+provid = s.tinyurl.short(link)
+print(f" Your short link is : {provid}")
+  
+
+
